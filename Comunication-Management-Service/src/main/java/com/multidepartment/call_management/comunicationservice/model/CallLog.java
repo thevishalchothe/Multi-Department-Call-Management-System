f2d82a2540cfa93cadc026d23e7dd149f91706e3
Vishal Chothe <163386224+thevishalchothe@users.noreply.github.com>
2024-12-23 22:10:00 +0530

    Updated model classes in CMS Service!

--- Comunication-Management-Service/src/main/java/com/multidepartment/call_management/comunicationservice/model/CallLog.java
+++ Comunication-Management-Service/src/main/java/com/multidepartment/call_management/comunicationservice/model/CallLog.java
@@ -1,12 +1,26 @@
 package com.multidepartment.call_management.comunicationservice.model;
 
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Document(collection = "call_logs") // This specifies the MongoDB collection name
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "call_logs")
 public class CallLog {
 	
-   
+    @Id
+    private String id;
+    private String sourceNumber;        // Caller number
+    private String destinationNumber;   // Receiver/department
+    private LocalDateTime callTime;     // Timestamp of the call
+    private String callType;            // NORMAL, EMERGENCY, ROUTED
+    
 }
+
--- Comunication-Management-Service/src/main/java/com/multidepartment/call_management/comunicationservice/model/CallRouting.java
+++ Comunication-Management-Service/src/main/java/com/multidepartment/call_management/comunicationservice/model/CallRouting.java
@@ -1,11 +1,10 @@
 package com.multidepartment.call_management.comunicationservice.model;
 
-import org.springframework.data.mongodb.core.mapping.Document;
-
 import lombok.Data;
 
-@Document(collection = "call_routing")
 @Data
 public class CallRouting {
-	
+
+	private String sourceNumber; // Caller number
+	private String extension; // Department extension dialed
 }
